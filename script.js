465efaa12d44acb9ee62623f230f142e731931f1
Evgeniy B <beliy-j@mail.ru>
2023-01-23 04:06:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let userName = 'Art';
+let userAge = 31;
+console.log(userName + ' is ' + userAge + ' years old');
+console.log(userHeight); // undefined
+userHeight = 170; // число - значение типа Number
+console.log(userHeight); // 170
